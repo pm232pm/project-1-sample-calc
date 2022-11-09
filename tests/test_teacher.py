eb090f9d63b3f935eeab898368b611d86e8266b6
pm232pm <113121633+pm232pm@users.noreply.github.com>
2022-11-08 20:53:53 -0500

    Update test_teacher.py

--- tests/test_teacher.py
+++ tests/test_teacher.py
@@ -1,11 +1,15 @@
 """This is the starting test file"""
 import os
 
+from app import main
+
 
 def test_main():
     # pylint: disable=line-too-long
     """My Main Test"""
+    main()
     base_dir = os.path.dirname(os.path.abspath(__file__))
+    print("bd", base_dir)
     sample_files_output_dir = os.path.join(base_dir, '..', 'sample_files_output')
     sample_files_output_directory_contents = os.listdir(sample_files_output_dir)
     assert len(sample_files_output_directory_contents) - 1 == 15, "There is an incorrect number of files"
